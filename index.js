742a30bb9e892349d4483664e9fda90cc4d3a687
Edward Angelo Salas <zyro670@gmail.com>
2022-03-19 21:43:59 -0400

    commit tree bfdab72ac8a135e6a422e2621954d6af269a4a6d

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
 
 // Write your code here
+var num1 = 31;
+var num2 = 2;
+var multiply = num1 * num2;
+var random = Math.floor(Math.random() * 10) + 1;
+var num3 = 12;
+var num4 = 3;
+var mod = num3 / num4;
+var max = 20;
